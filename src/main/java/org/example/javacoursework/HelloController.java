85d0e674e32e57a5e4be7ed2edc0657bc22644ec
Wasif Asi <wasifasi01@gmail.com>
2024-04-15 23:55:06 +0530

    First push with grid

--- src/main/java/org/example/javacoursework/HelloController.java
+++ src/main/java/org/example/javacoursework/HelloController.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
